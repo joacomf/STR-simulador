411ec7c5c4069191cb3460f8d8e257c904ee9222
cybercatnet <cybercat_666_78@hotmail.com>
2019-11-16 10:49:35 -0300

    Procesador deberia tener 2 tareas registradas al asignarle 2 tareas

--- spec/procesador_spec.rb
+++ spec/procesador_spec.rb
@@ -14,4 +14,12 @@ describe Procesador do
 
     expect(procesador.cantidad_tareas).to eq 1
   end
+
+  it 'deberia tener 2 tareas registradas al asignarle 2 tareas' do
+    tarea = Tarea.new(10)
+    procesador.tarea(tarea)
+    procesador.tarea(tarea)
+
+    expect(procesador.cantidad_tareas).to eq 2
+  end
 end
