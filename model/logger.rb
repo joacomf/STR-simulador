645274881a62449611eba3d8ffe3317351374326
Joaco <joaquinamf@gmail.com>
2019-11-24 13:33:35 -0300

    Crea Logger para ejecuciones

---
+++ model/logger.rb
@@ -0,0 +1,5 @@
+class Logger
+  def self.ejecucion(tiempo_actual, ejecucion_actual, ejecuciones_totales)
+    puts "Tiempo #{tiempo_actual}. Ejecución de #{ejecucion_actual} de tarea #1. Total #{ejecuciones_totales}"
+  end
+end
