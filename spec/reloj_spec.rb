afd97362beed93476327f279e94777736a9c5492
Germán Torós <cybercat_666_78@hotmail.com>
2019-11-24 13:03:33 -0300

    Reloj deberia crearse con tiempo definido si es especificado

--- spec/reloj_spec.rb
+++ spec/reloj_spec.rb
@@ -4,7 +4,12 @@ require_relative '../model/reloj'
 describe Reloj do
   let(:reloj) { described_class.new }
 
-  it 'deberia crearse con tiempo en 0' do
+  it 'deberia crearse con tiempo en 0 si no es especificado' do
     expect(reloj.tiempo).to eq 0
+  end
+
+  it 'deberia crearse con tiempo definido si es especificado' do
+    reloj = described_class.new(15)
+    expect(reloj.tiempo).to eq 15
   end
 end
