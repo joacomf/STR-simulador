fa0fe83f6975a77a5b2b1c5c05cbe2c801eac5f9
Joaco <joaquinamf@gmail.com>
2019-11-24 20:27:31 -0300

    Agrega planificador FIFO

---
+++ model/planificador_fifo.rb
@@ -0,0 +1,4 @@
+require_relative './programador'
+
+class PlanificadorFIFO < Programador
+end
---
+++ spec/planificador_fifo_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require_relative '../model/planificador_fifo'
+require_relative '../model/procesador'
+require_relative '../model/tarea'
+
+describe PlanificadorFIFO do
+  let(:procesador) { Procesador.new }
+  let(:planificadorFIFO) { described_class.new(procesador) }
+
+  before(:each) do
+    tarea1 = Tarea.new(tiempo: 5, periodo: 10)
+    tarea2 = Tarea.new(tiempo: 3, periodo: 15)
+
+    planificadorFIFO.encolar(tarea1)
+    planificadorFIFO.encolar(tarea2)
+  end
+
+  it 'debe ejecutar por 2 periodos las 2 tareas en orden de llegada' do
+    planificadorFIFO.simular(2)
+
+    expect(planificadorFIFO.tareas[0].ciclos_completados).to eq 2
+    expect(planificadorFIFO.tareas[1].ciclos_completados).to eq 2
+  end
+end
