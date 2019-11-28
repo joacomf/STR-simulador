d4b392087c652107096fc45e0f96cf54dd7e7c89
Joaco <joaquinamf@gmail.com>
2019-11-28 10:30:26 -0300

    Mueve simular y procesar a FIFO

--- model/planificador.rb
+++ model/planificador.rb
@@ -25,30 +25,9 @@ class Planificador
     @tareas.push(tarea)
   end
 
-  def procesar(tarea)
-    until tarea.es_ejecutable?
-      return if simulacion_finalizada?
+  def procesar(tarea); end
 
-      @procesador.procesar(@nop)
-    end
-    tarea.tiempo.times do
-      return if simulacion_finalizada?
-
-      @procesador.procesar(tarea)
-    end
-  end
-
-  def simular(max_ciclos = 200)
-    @max_ciclos = max_ciclos
-    until simulacion_finalizada?
-      tarea = @tareas.pop
-      procesar(tarea)
-      @tareas.push(tarea)
-      return if simulacion_finalizada?
-
-      @ejecutadas += 1
-    end
-  end
+  def simular(max_ciclos = 200); end
 
   def simulacion_finalizada?
     @max_ciclos <= @reloj.tiempo
--- model/planificador_fifo.rb
+++ model/planificador_fifo.rb
@@ -1,4 +1,28 @@
 require_relative './planificador'
 
 class PlanificadorFIFO < Planificador
+  def procesar(tarea)
+    until tarea.es_ejecutable?
+      return if simulacion_finalizada?
+
+      @procesador.procesar(@nop)
+    end
+    tarea.tiempo.times do
+      return if simulacion_finalizada?
+
+      @procesador.procesar(tarea)
+    end
+  end
+
+  def simular(max_ciclos = 200)
+    @max_ciclos = max_ciclos
+    until simulacion_finalizada?
+      tarea = @tareas.pop
+      procesar(tarea)
+      @tareas.push(tarea)
+      return if simulacion_finalizada?
+
+      @ejecutadas += 1
+    end
+  end
 end
