20885c299dd8d25d4c45e42fb19285cbf8f79480
Joaco <joaquinamf@gmail.com>
2019-11-30 01:41:45 -0300

    Agrega cobertura a los logs

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -7,4 +7,12 @@ describe Logger do
   it 'debería imprimir ejecucion de tarea ' do
     expect { described_class.ejecucion(3, 3, 5, 1) }.to output("Tiempo 3. Ejecución de 3 de tarea #1. Total 5\n").to_stdout
   end
+
+  it 'debería imprimir excepcion ' do
+    expect { described_class.nop(4) }.to output("Tiempo 4. Nop ejecutado\n").to_stdout
+  end
+
+  it 'debería imprimir excepcion' do
+    expect { described_class.excepcion('Excepcion lanzada') }.to output("Excepcion lanzada\n").to_stdout
+  end
 end
