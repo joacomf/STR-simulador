991f9feee7730839e015800117f9d6221d6adc80
Joaco <joaquinamf@gmail.com>
2019-11-30 00:49:21 -0300

    Agrega cobertura a test de planificador por prioridad

--- spec/planificador_por_prioridad_spec.rb
+++ spec/planificador_por_prioridad_spec.rb
@@ -4,7 +4,7 @@ require_relative '../model/procesador'
 require_relative '../model/tarea'
 
 describe PlanificadorPorPrioridad do
-  it 'debe ejecutar 1 vez las 2 tareas en un ciclo' do
+  it 'debe ejecutar 3 de las 2 tareas en un ciclo' do
     procesador = Procesador.new
     planificador_fifo = described_class.new(procesador)
     tarea1 = Tarea.new(tiempo_inicio: 5, tiempo: 10, periodo: 40, reloj: procesador.reloj, nombre: 'Leer', prioridad: 1)
@@ -21,5 +21,19 @@ describe PlanificadorPorPrioridad do
     expect(tarea1.ciclos_completados).to eq 1
     expect(tarea2.ciclos_completados).to eq 1
     expect(tarea4.ciclos_completados).to eq 0
+  end
+
+  it 'debe ejecutar 1 vez las 2 tareas en un ciclo' do
+    procesador = Procesador.new
+    planificador_fifo = described_class.new(procesador)
+    tarea1 = Tarea.new(tiempo_inicio: 0, tiempo: 10, periodo: 40, reloj: procesador.reloj, nombre: 'Leer', prioridad: 2)
+    tarea2 = Tarea.new(tiempo_inicio: 4, tiempo: 10, periodo: 40, reloj: procesador.reloj, nombre: 'Escribir', prioridad: 1)
+
+    planificador_fifo.encolar(tarea1)
+    planificador_fifo.encolar(tarea2)
+    planificador_fifo.simular(30)
+
+    expect(tarea1.ciclos_completados).to eq 1
+    expect(tarea2.ciclos_completados).to eq 1
   end
 end
