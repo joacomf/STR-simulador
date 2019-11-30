c0d53b76aa9f64d26b543b5438efcbaa40387007
Germán Torós <cybercat_666_78@hotmail.com>
2019-11-30 04:33:44 -0300

    Refactor

--- spec/planificador_por_prioridad_spec.rb
+++ spec/planificador_por_prioridad_spec.rb
@@ -6,17 +6,17 @@ require_relative '../model/tarea'
 describe PlanificadorPorPrioridad do
   it 'debe ejecutar 3 de las 2 tareas en un ciclo' do
     procesador = Procesador.new
-    planificador_fifo = described_class.new(procesador)
+    planificador_por_prioridad = described_class.new(procesador)
     tarea1 = Tarea.new(tiempo_inicio: 5, tiempo: 10, periodo: 40, nombre: 'Leer', prioridad: 1)
     tarea2 = Tarea.new(tiempo_inicio: 12, tiempo: 10, periodo: 40, nombre: 'Escribir', prioridad: 2)
     tarea3 = Tarea.new(tiempo_inicio: 0, tiempo: 10, periodo: 40, nombre: 'Calcular', prioridad: 3)
     tarea4 = Tarea.new(tiempo_inicio: 18, tiempo: 10, periodo: 40, nombre: 'Expulsar', prioridad: 4)
 
-    planificador_fifo.encolar(tarea1)
-    planificador_fifo.encolar(tarea2)
-    planificador_fifo.encolar(tarea3)
-    planificador_fifo.encolar(tarea4)
-    planificador_fifo.simular(30)
+    planificador_por_prioridad.encolar(tarea1)
+    planificador_por_prioridad.encolar(tarea2)
+    planificador_por_prioridad.encolar(tarea3)
+    planificador_por_prioridad.encolar(tarea4)
+    planificador_por_prioridad.simular(30)
 
     expect(tarea1.ciclos_completados).to eq 1
     expect(tarea2.ciclos_completados).to eq 1
@@ -25,13 +25,13 @@ describe PlanificadorPorPrioridad do
 
   it 'debe ejecutar 1 vez las 2 tareas en un ciclo' do
     procesador = Procesador.new
-    planificador_fifo = described_class.new(procesador)
+    planificador_por_prioridad = described_class.new(procesador)
     tarea1 = Tarea.new(tiempo_inicio: 0, tiempo: 10, periodo: 40, nombre: 'Leer', prioridad: 2)
     tarea2 = Tarea.new(tiempo_inicio: 4, tiempo: 10, periodo: 40, nombre: 'Escribir', prioridad: 1)
 
-    planificador_fifo.encolar(tarea1)
-    planificador_fifo.encolar(tarea2)
-    planificador_fifo.simular(30)
+    planificador_por_prioridad.encolar(tarea1)
+    planificador_por_prioridad.encolar(tarea2)
+    planificador_por_prioridad.simular(30)
 
     expect(tarea1.ciclos_completados).to eq 1
     expect(tarea2.ciclos_completados).to eq 1
